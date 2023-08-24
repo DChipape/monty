683d081c23ec907afd734f71eefa4ff1c09eab82
dchipape <dimakatsosharon94@gmail.com>
2023-08-24 08:51:59 -0700

    our project

---
+++ queue.c
@@ -0,0 +1,51 @@
+#include "monty.h"
+
+/**
+ * f_queue - witch to the queue mode (FIFO)
+ * @head: stack head poiter (unused)
+ * @counter: line number (unuse)
+ * Return: no return value
+*/
+
+void f_queue(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 1;
+}
+
+/**
+ * addqueue - add node to the rear of queue
+ * @head: head of queue pointer
+ * @n: The value to be added to the new node
+ * Return: no return value
+*/
+
+void addqueue(stack_t **head, int n)
+{
+	stack_t *new_node, *aux;
+
+	aux = *head;
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+	{
+		printf("Error\n");
+	}
+	new_node->n = n;
+	new_node->next = NULL;
+	if (aux)
+	{
+		while (aux->next)
+			aux = aux->next;
+	}
+	if (!aux)
+	{
+		*head = new_node;
+		new_node->prev = NULL;
+	}
+	else
+	{
+		aux->next = new_node;
+		new_node->prev = aux;
+	}
+}
